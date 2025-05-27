db27c16bd025629e6a26066387e79b0805c17f77
AK <i221545@gmail.com>
2025-05-28 03:13:38 +0500

    please get work

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-import { GoogleTagManager } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -42,7 +41,6 @@ export default async function RootLayout({ children }) {
         </main>
         <Footer />
       </body>
-      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
     </html>
   );
 }
