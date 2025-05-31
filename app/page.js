d775763116143a42e697914022d4356f6d71ede4
AK <i221545@gmail.com>
2025-05-31 23:27:48 +0500

    testing 1

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ import { PortfolioDataContext } from "./context/PortfolioDataContext";
 import AboutSection from "./components/homepage/about";
 
 import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
+// import Education from "./components/homepage/education";
+// import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
