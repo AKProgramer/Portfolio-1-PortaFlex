7e91009507882c927217aba42e7cf9d5a4a38cf2
AK <i221545@gmail.com>
2025-06-01 00:03:58 +0500

    testing 5

--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
-"use client"
+"use client";
+
 import { useContext } from "react";
 import { PortfolioDataContext } from "./context/PortfolioDataContext";
 import AboutSection from "./components/homepage/about";
-
+import dynamic from "next/dynamic";
+const Education = dynamic(
+  () => import('./components/homepage/education'),
+  { ssr: false }
+);
+const Experience = dynamic(
+  () => import('./components/homepage/experience'),
+  { ssr: false }
+);
 import ContactSection from "./components/homepage/contact";
-// import Education from "./components/homepage/education";
-// import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
@@ -15,10 +22,16 @@ import Skills from "./components/homepage/skills";
 export default function Home() {
   const portfolioData = useContext(PortfolioDataContext);
   const sectionTypes = (portfolioData.sections || []).map(s => s.type);
-console.log(sectionTypes.length)
+
   return (
-    <>
-     <div><h1>Hellow Worls</h1></div>
-    </>
+    <div suppressHydrationWarning >
+      {sectionTypes.includes("hero") && <HeroSection />}
+      {sectionTypes.includes("about") && <AboutSection />}
+      {sectionTypes.includes("experience") && <Experience />}
+      {sectionTypes.includes("skills") && <Skills />}
+      {sectionTypes.includes("projects") && <Projects />}
+      {sectionTypes.includes("education") && <Education />}
+      {sectionTypes.includes("contact") && <ContactSection />}
+    </div>
   )
-}; 
+};
